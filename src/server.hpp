2cca9f5eb04921d13d45b75a8bf4b46729667d01
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2026-04-17 19:40:58 +0300

    Update server.hpp

--- src/server.hpp
+++ src/server.hpp
@@ -8,5 +8,5 @@ class Server {
          sockaddr_in Server_addr {};
      public:
          bool Start(const int &port);
-         void Info_Sender(const int &SV_sock, const int &CL_sock, char *buff);
+         void Info_Sender(const int &CL_sock, char *buff);
 };                     
